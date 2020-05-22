9b552c5a371805d86a63ada91a9cc8bed246a0b0
Solomon Irving <solomon.irving@gmail.com>
2020-05-21 18:46:12 -0700

    first commit

---
+++ media.js
@@ -0,0 +1,16 @@
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, and Opera
+}
