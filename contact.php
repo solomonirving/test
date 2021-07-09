d5bbf9687032ee0da7e0fac0a2bfab843ee52286
Solomon Irving <solomon.irving@gmail.com>
2021-07-09 16:32:15 -0700

    a

--- contact.php
+++ contact.php
@@ -1,34 +1,27 @@
 <?php
 /*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
-
-require 'PHPMailer-master/PHPMailerAutoload.php';
-
-/*
-*  CONFIGURE EVERYTHING HERE
-*/
+ *  CONFIGURE EVERYTHING HERE
+ */
 
 // an email address that will be in the From field of the email.
-$fromEmail = 'demo@domain.com';
-$fromName = 'Demo contact form';
+$from = 'Demo contact form <tyler.7thladderind@gmail.com>';
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'demo@domain.com';
-$sendToName = 'Demo contact form';
+$sendTo = 'Demo contact form <tyler.7thladderind@gmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
+
 /*
  *  LET'S DO THE SENDING
  */
@@ -59,7 +52,7 @@ try
     
     // Send email
     mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
+    header('Location: home.html');
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
 catch (\Exception $e)
